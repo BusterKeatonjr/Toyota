0a9e953f10ba3800ab6f472bccef3ccbc5b04258
BusterKeatonjr <evgexa0007@gmail.com>
2022-11-23 16:54:26 +0300

    23.11

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/CAMRY', function(req, res, next) {
+  res.send("<h1>Страница Camry</h1>")
+});
+router.get('/CROWN', function(req, res, next) {
+  res.send("<h1>Страница Crown</h1>")
+});
+router.get('/PRIUS', function(req, res, next) {
+  res.send("<h1>Страница Prius</h1>")
 });
 
 module.exports = router;
