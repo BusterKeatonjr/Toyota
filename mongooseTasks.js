8c5702d534e330a0310cb47e576733db3dccc44d
BusterKeatonjr <evgexa0007@gmail.com>
2022-11-30 14:53:41 +0300

    30.11

--- mongooseTasks.js
+++ mongooseTasks.js
@@ -1,13 +1,16 @@
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/test')
 
-var car = mongoose.model('car', { name: String })
+var schema = mongoose.Schema({ name: String })
 
-var cars = new car({ name: 'Машина' })
-cars.save(function (err) {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log('Врум')
-    }
+schema.methods.meow = function(){
+    console.log(this.get("name") + " сделал врум")
+}
+
+var car = mongoose.model('car', schema)
+
+var kitty = new car({ name: 'Машина' })
+kitty.save(function (err) {
+    kitty.meow()
 })
+
