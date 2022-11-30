c54a91ae8ab214b7400d606dc4619d4d233102ba
BusterKeatonjr <evgexa0007@gmail.com>
2022-11-30 13:48:45 +0300

    30.11

---
+++ createDB.js
@@ -0,0 +1,18 @@
+var MongoClient = require('mongodb').MongoClient
+
+const uri = "mongodb://localhost:27017/"
+const client = new MongoClient(uri)
+async function run() {
+try {
+await client.connect();
+var database = client.db("toyota");
+database.dropDatabase()
+database = client.db("toyota");
+const cats = database.collection("cars");
+const result = await cats.insertOne({name:"Машина"});
+console.log(`${result} documents were inserted`);
+} finally {
+await client.close();
+}
+}
+run()
