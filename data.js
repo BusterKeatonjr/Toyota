d9e2941d1b7ee14b54b6ed7b0a86683214d4293a
BusterKeatonjr <evgexa0007@gmail.com>
2022-11-30 14:37:18 +0300

    30.11

---
+++ data.js
@@ -0,0 +1,23 @@
+var data = [{
+  title: 'Camry',
+  nick: 'CAMRY',
+  avatar: '/images/Camry.jpg',
+  desc: 'Стильный дизайн с динамичными линиями кузова, эффектная форма переднего бампера, решетка радиатора с боковой серебристой окантовкой, новые цвета экстерьера и уникальный двухцветный окрас кузова для специальной серии GR SPORT — все это подчеркивает выразительный облик бизнес-седана.'
+  },
+  {
+  title: 'Crown',
+  nick: 'CROWN',
+  avatar: '/images/Crown.jpg',
+  desc: ''
+  },
+  {
+  title: 'Prius',
+  nick: 'PRIUS',
+  avatar: '/images/Prius.jpg',
+  desc: ''
+  }
+  ];
+  
+  
+  module.exports.data = data;
+  
