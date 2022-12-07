957ba895f25ced3aad82d0b8dcb56fe808a09845
BusterKeatonjr <evgexa0007@gmail.com>
2022-12-07 16:47:54 +0300

    07.12

--- routes/cars.js
+++ routes/cars.js
@@ -6,8 +6,15 @@ router.get('/', function(req, res, next) {
   res.send('Новый маршрутизатор, для маршрутов, начинающихся с cars');
 });
 
-module.exports = router;
-/*Страница car */
-router.get("/:nick", function(req, res, next) {
-  res.send(req.params.nick);
-});
+/* Страница машин */
+router.get('/:nick', function(req, res, next) {
+  Cat.findOne({nick:req.params.nick}, function(err,car){
+      if(err) return next(err)
+      if(!car) return next(new Error("Такой машины не существует"))
+      res.render('car', {
+          title: car.title,
+          picture: car.avatar,
+          desc: car.desc
+      })
+  })
+})
