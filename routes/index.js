2b3c5c5167cac566b19738c001575617ce5f358d
BusterKeatonjr <evgexa0007@gmail.com>
2022-12-14 12:44:42 +0300

    14.12

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,9 @@ var Car = require("../models/car").Car
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    Car.find({},{_id:0,title:1,nick:1},function(err,menu){
-        res.render('index', {
-                                title: 'Express',
-                                menu: menu
-                            });
-    })
-
+  res.cookie('greeting', 'Hi!!!').render('index', { title: 'Express', menu:menu });
 });
+
 
 /* Страница Camry */
 router.get('/CAMRY', function(req, res, next) {
